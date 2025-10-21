6cb0d54f26c3c00b4ae2073c822a3efdefa243d4
Chase Blissett <chaseblissett21@gmail.com>
2025-10-20 22:46:29 -0500

    Add circular linked list implementation

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,6 @@ public class Main {
         node.next.next = new SinglyLinkedList(3);
 
         SinglyLinkedList.printSinglyLinkedList(node);
-
         System.out.println("\n" + node.value);
         /* Because this method changes what the node object points to
          * in the line:       node = node.next;
@@ -51,5 +50,24 @@ public class Main {
         DoublyLinkedList.print(node1);
         System.out.println("\nBackwards:");
         DoublyLinkedList.printBackwards(node3);
+
+        System.out.println("\n----------------------------------------------\n");
+        System.out.println("CircularLinkedList:\n");
+
+        SinglyLinkedList circularLinkedListNode1 = new SinglyLinkedList(1);
+        SinglyLinkedList circularLinkedListNode2 = new SinglyLinkedList(2);
+        SinglyLinkedList circularLinkedListNode3 = new SinglyLinkedList(3);
+        SinglyLinkedList circularLinkedListNode4 = new SinglyLinkedList(4);
+        SinglyLinkedList circularLinkedListNode5 = new SinglyLinkedList(5);
+        SinglyLinkedList circularLinkedListNode6 = new SinglyLinkedList(6);
+        circularLinkedListNode1.next = circularLinkedListNode2;
+        circularLinkedListNode2.next = circularLinkedListNode3;
+        circularLinkedListNode3.next = circularLinkedListNode4;
+        circularLinkedListNode4.next = circularLinkedListNode5;
+        circularLinkedListNode5.next = circularLinkedListNode6;
+        circularLinkedListNode6.next = circularLinkedListNode1;
+
+        SinglyLinkedList.printCircularLinkedList(circularLinkedListNode1);
+
     }
 }
--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -13,4 +13,17 @@ public class SinglyLinkedList {
             node = node.next;
         }
     }
+
+    public static void printCircularLinkedList(SinglyLinkedList head) {
+        if (head == null) {
+            System.out.println("Linked List is empty");
+            return;
+        }
+        SinglyLinkedList current = head;
+        do {
+            System.out.print(current.value + " ");
+            current = current.next;
+        } while (current != head);
+
+    }
 }
